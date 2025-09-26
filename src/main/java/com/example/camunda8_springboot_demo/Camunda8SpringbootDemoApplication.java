04621235a81208ae9f49410db9a5272dff86b846
Alex Belke <233420507+alexbelkedev@users.noreply.github.com>
2025-09-26 13:05:27 +0200

    init commit

---
+++ src/main/java/com/example/camunda8_springboot_demo/Camunda8SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda8_springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camunda8SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Camunda8SpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camunda8_springboot_demo/Camunda8SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camunda8_springboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camunda8SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
